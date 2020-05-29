6bb3fd3c1acfea6716a2fdcafb6599238af6bc65
Kit Transue <src@kittransue.net>
2020-05-29 16:45:28 -0700

    Conditionalize for non-Apple, but make note that it's easy to stub Linux I2C ioctl structures.

--- Sources/LinuxI2CDev/I2CUmbrella.h
+++ Sources/LinuxI2CDev/I2CUmbrella.h
@@ -1,3 +1,7 @@
+#ifdef __APPLE__
+// the I2C headers are fairly self contained; it is possible to stub them here
+#else
 #include <linux/types.h>
 #include <linux/i2c.h>
 #include <linux/i2c-dev.h>
+#endif
