53941791a8d53fd65141a652982712be7c6929bb
Kit Transue <src@kittransue.net>
2020-06-04 17:35:25 -0700

    ShiftLED: add color-based index mechanism

--- Sources/ShiftLED/ShiftLED.swift
+++ Sources/ShiftLED/ShiftLED.swift
@@ -15,13 +15,16 @@ import LEDUtils
 public class ShiftLED {
     let count: Int
     let bus: LinkSPI
+    var buffer: Data
+    var current: Double
 
     public init(bus: LinkSPI, stringLength: Int) {
         count = stringLength
         self.bus = bus
+        buffer = Data(repeating: 0, count: (count + 1) * 4)
+        buffer.append(Data(repeating: 0xff, count: 4))
+        current = 0.25
     }
-
-
 
     func encode(color: LEDColor, current: Double) -> [UInt8] {
         let r8 = UInt8(clamping: Int(color.red * 255))
@@ -34,19 +37,30 @@ public class ShiftLED {
         return [c8, b8, g8, r8]
     }
 
-    public func all(color: LEDColor, current: Double) {
-        var buffer = Data()
+    func decode(data: Data) -> LEDColor {
+        //let current5 = data[0] & 0b0001_1111
+        let blue8 = data[1]
+        let green8 = data[2]
+        let red8 = data[3]
+        return LEDColor(red: Double(red8) / Double(UInt8.max),
+                        green: Double(green8) / Double(UInt8.max),
+                        blue: Double(blue8) / Double(UInt8.max))
+    }
 
-        // start: all zeros
-        buffer.append(contentsOf: repeatElement(UInt8(0), count: 4))
-
-        for _ in 0..<count {
-            buffer.append(contentsOf: encode(color: color, current: current))
+    subscript(index: Int) -> LEDColor {
+        get {
+            decode(data: buffer.subdata(in: (index + 1) * 4 ..< (index + 2) * 4))
         }
+        set {
+            buffer.replaceSubrange( ((index + 1) * 4) ..< ((index + 2) * 4), with: encode(color: newValue, current: current))
+        }
+    }
 
-        // end: all ones
-        buffer.append(contentsOf: repeatElement(UInt8(0xff), count: 4))
-
+    public func all(color: LEDColor, current: Double) {
+        self.current = current
+        for index in 0..<count {
+            self[index] = color
+        }
         bus.write(data: buffer, count: buffer.count)
     }
 
