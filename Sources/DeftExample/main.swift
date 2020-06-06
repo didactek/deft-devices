95f7ab964a604e7b7d10e50ab8f67b886b8150ca
Kit Transue <src@kittransue.net>
2020-06-05 18:25:32 -0700

    UNTESTED: tempMonitorFade in example main.

--- Sources/DeftExample/main.swift
+++ Sources/DeftExample/main.swift
@@ -62,7 +62,8 @@ do {  // provide a scope for the ssh-availability guard
     let ledCount = 72
     let leds = ShiftLED(bus: spi, stringLength: ledCount, current: 0.3)
 
-    twoSegmentFade(leds: leds, ledCount: ledCount)
+//    twoSegmentFade(leds: leds, ledCount: ledCount)
+    tempMonitorFade(sensor: temp, leds: leds, ledCount: ledCount)
 
     leds.clear()
 }
@@ -105,3 +106,71 @@ func twoSegmentFade(leds: ShiftLED, ledCount: Int) {
     }
 }
 
+func tempMonitorFade(sensor: MCP9808_TemperatureSensor, leds: ShiftLED, ledCount: Int) {
+    let history = TimeAndTemperature()
+    let resolution = 100
+
+    for _ in 0 ..< 1000 {
+        history.recordObservation(temperature: sensor.temperature)
+
+        let hot = LEDColor.randomSaturated()
+        let cold = LEDColor.randomSaturated()
+
+        let range = colorFade(from: cold, to: hot, count: resolution)
+        let observations = history.averages().prefix(ledCount)
+        let lowTemp = observations.min()!
+        let hiTemp = observations.max()!
+
+        let indices = observations.map { (hiTemp == lowTemp) ? 0 : Int( Double(resolution) * ($0 - lowTemp / (hiTemp - lowTemp))) }
+
+        for (index, observation) in indices.enumerated() {
+            leds[index] = range[observation]
+        }
+        leds.flushUpdates()
+        usleep(300_000)
+    }
+}
+
+/// Log time and temperature and report averages over some period
+class TimeAndTemperature {
+    class Averager {
+        var sampleCount: Int = 0
+        var cumulative: Double = 0
+
+        func record(entry: Double) {
+            cumulative += entry
+            sampleCount += 1
+        }
+
+        init(firstValue: Double) {
+            record(entry: firstValue)
+        }
+
+        func average() -> Double {
+            return cumulative / Double(sampleCount)
+        }
+    }
+
+    var record: [Int : Averager] = [:]
+
+    func recordObservation(temperature: Double, atTime: Date = Date()) {
+        // FIXME: maybe clear entries rolling forward?
+        let midnight = Calendar.current.startOfDay(for: atTime)
+        let atMinute = Int(atTime.timeIntervalSince(midnight) / 2) // FIXME: 60
+
+        if let accumulator = record[atMinute] {
+            accumulator.record(entry: temperature)
+        }
+        else {
+            record[atMinute] = Averager(firstValue: temperature)
+        }
+    }
+
+    // FIXME: split by blocks of minutes
+    func averages() -> [Double] {
+        let sorted = record.sorted { kv1, kv2 in
+            return kv1.key < kv2.key
+        }
+        return sorted.map {_, value in value.average() }
+    }
+}
