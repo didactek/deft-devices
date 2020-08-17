782412a6b9fea52539ffc57e3583096fc5e5c9c2
Kit Transue <src@kittransue.net>
2020-08-16 19:15:30 -0700

    FTDI I2C and SPI buses are mutually exclusive

--- Sources/LinuxI2CDev/I2CUmbrella.h
+++ Sources/LinuxI2CDev/I2CUmbrella.h
@@ -1,5 +1,8 @@
 #ifdef __APPLE__
 // the I2C headers are fairly self contained; it is possible to stub them here
+#include "linux-headers/linux/types.h"
+#include "linux-headers/linux/i2c.h"
+#include "linux-headers/linux/i2c-dev.h"
 #else
 #include <linux/types.h>
 #include <linux/i2c.h>
